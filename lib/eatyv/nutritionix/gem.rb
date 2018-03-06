5d1593e96fdd58ebffb0b12c076e656e759cf656
Salanoid <salanoid@gmail.com>
2018-03-06 23:31:00 +0200

    display with puts all foods similar and number of calories

--- lib/eatyv/nutritionix/gem.rb
+++ lib/eatyv/nutritionix/gem.rb
@@ -2,30 +2,34 @@ require "eatyv/nutritionix/gem/version"
 require "httparty"
 
 module Eatyv
+
   include HTTParty
+
   class GetNutritionalValues
     def initialize(food)
       @food = food
 
       headers = {
-        'x-app-id' => 'b479cfcf',
-        'x-app-key' => '83ebc7a194ab072f8c4be2b3dba82732',
-        'content-type' => 'application/json'
-      } 
-      
-      body = {
-        "query" => @food,
+          'x-app-id' => 'b479cfcf',
+          'x-app-key' => '83ebc7a194ab072f8c4be2b3dba82732',
+          'content-type' => 'application/json'
       }
-      
+
+      body = {
+          "query" => @food,
+      }
+
       @nutritionix = HTTParty.get(
-        "https://trackapi.nutritionix.com/v2/search/instant/?query=#{body}", 
-        :headers => headers
+          "https://trackapi.nutritionix.com/v2/search/instant/?query=#{body}",
+          :headers => headers
       )
 
-      @nutritionix.each do |nutritioni| 
+      @nutritionix.each do |nutritioni|
         nutritioni[1].each do |nutr|
-          nutr['food_name']   
-        end 
+          puts nutr['food_name']
+          puts nutr['nf_calories']
+          puts '\n\n\n\n\n\n#################################################################\n\n\n\n'
+        end
       end
 
     end
